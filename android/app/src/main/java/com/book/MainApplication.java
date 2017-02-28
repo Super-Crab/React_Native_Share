22352d3d9604061eab6df68bb0fd8dd382e9976c
解佩旋 <peixuan.xie@unspay.com>
2017-02-28 15:04:56 +0800

    增加自定义title

--- android/app/src/main/java/com/book/MainApplication.java
+++ android/app/src/main/java/com/book/MainApplication.java
@@ -2,7 +2,6 @@ package com.book;
 
 import android.app.Application;
 
-import com.book.module.ToastCustomPackage;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -22,8 +21,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-              new ToastCustomPackage()
+          new MainReactPackage()
       );
     }
   };
